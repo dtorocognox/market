280a654a397fff79be5dba7211a8ac8305bbf6a9
David Toro Alzate <dtoro@cognox.com>
2016-11-10 12:02:16 -0500

    averiguar entrar login angular

--- js/code.js
+++ js/code.js
@@ -1,22 +1,51 @@
 var app = angular.module('market', ['ui.router']);
 
-app.config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/login");
-
-    app.controller('login', function ($scope, $http) {
-        $http.get('../data.json')
-            .success(function (data) {
-                console.log(data);
-            });
+app.controller('login', function ($scope, $http) {
+    $http.get("data.json")
+        .success(function (data) {
+            $scope.data = data;
+            var datas = data;
+        })
+    .error(function (err) {
+        var datas = err;
+        console.log(datas);
     });
 
+    $scope.homes = "login";
 
+    $scope.password = function (pass) {
+        if($scope.data.password == pass){
+            $scope.homes = "home";
+            console.log("Sí");
+            return "home";
+        }
+
+    }
+    $scope.go = function(state) {
+        $state.go(state);
+        console.log("entro");
+    }
+});
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/login");
     $stateProvider
         .state('login', {
             url: "/login",
             views:{
                 "subview1":{
                     templateUrl: "html/login.html"
+                }
+            }
+        })
+        .state('home',{
+           url: "/home",
+            views:{
+                "subview1":{
+                    templateUrl: "html/sidebar.html"
+                },
+                "subview2":{
+                    templateUrl: "html/home.html"
                 }
             }
         });
