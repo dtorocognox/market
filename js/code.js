9bddfe1f61e72f722e84743eb055cf5810fa028b
David Toro Alzate <dtoro@cognox.com>
2016-11-10 09:33:18 -0500

    plantillas terminadas

--- js/code.js
+++ js/code.js
@@ -1,3 +1,23 @@
-/**
- * Created by dtoro on 10/11/2016.
- */
+var app = angular.module('market', ['ui.router']);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/login");
+
+    app.controller('login', function ($scope, $http) {
+        $http.get('../data.json')
+            .success(function (data) {
+                console.log(data);
+            });
+    });
+
+
+    $stateProvider
+        .state('login', {
+            url: "/login",
+            views:{
+                "subview1":{
+                    templateUrl: "html/login.html"
+                }
+            }
+        });
+});
